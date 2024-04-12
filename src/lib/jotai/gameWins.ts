0e0cbbbf6890d2f1a29a325fa3b4d405c0bbf124
Sean Gray <seangray.dev@gmail.com>
2024-04-12 01:34:31 -0400

    Refactor gameWinsAtom to read and update data from localStorage

--- src/lib/jotai/gameWins.ts
+++ src/lib/jotai/gameWins.ts
@@ -1,3 +1,16 @@
 import { atom } from "jotai";
 
-export const gameWinsAtom = atom(new Map());
+function readGameWins() {
+  const item = localStorage.getItem("gameWins");
+  return item ? new Map(JSON.parse(item)) : new Map();
+}
+
+export const gameWinsAtom = atom(readGameWins(), (get, set, update) => {
+  const newWins =
+    typeof update === "function" ? update(get(gameWinsAtom)) : update;
+  set(gameWinsAtom, newWins);
+  localStorage.setItem(
+    "gameWins",
+    JSON.stringify(Array.from(newWins.entries())),
+  );
+});
