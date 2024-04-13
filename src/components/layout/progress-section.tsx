8096faad00341e8feb0c1f0dba3a2a58ed4b8a9e
Sean Gray <seangray.dev@gmail.com>
2024-04-13 09:26:04 -0400

    Add reset progress button and claim NFT button

--- src/components/layout/progress-section.tsx
+++ src/components/layout/progress-section.tsx
@@ -11,6 +11,7 @@ import {
 } from "@/components/ui/alert-dialog";
 import { gameWinsAtom } from "@/lib/jotai/gameWins";
 import { useAtom } from "jotai";
+import Link from "next/link";
 import { Button } from "../ui/button";
 import { Progress } from "../ui/progress";
 
@@ -27,32 +28,43 @@ export default function ProgressSection() {
     localStorage.removeItem("gameWins");
   };
 
+  const isAllGamesCompleted = winsCount === 5;
+
   return (
     <div className="flex flex-col gap-8">
       <Progress value={winsCount * 20} max={100} />
-      <AlertDialog>
-        <AlertDialogTrigger asChild>
-          <Button>Reset Progress</Button>
-        </AlertDialogTrigger>
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-            <AlertDialogDescription>
-              This action cannot be undone and you will lose your current
-              progress.
-            </AlertDialogDescription>
-          </AlertDialogHeader>
-          <AlertDialogFooter>
-            <AlertDialogCancel>Cancel</AlertDialogCancel>
-            <AlertDialogAction
-              onClick={handleResetProgress}
-              className="bg-destructive text-white hover:bg-destructive/80"
-            >
-              Reset
-            </AlertDialogAction>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
+      <div className="flex flex-col justify-between gap-2 md:flex-row">
+        <AlertDialog>
+          <AlertDialogTrigger asChild>
+            <Button className="w-full">Reset Progress</Button>
+          </AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+              <AlertDialogDescription>
+                This action cannot be undone and you will lose your current
+                progress.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <AlertDialogAction
+                onClick={handleResetProgress}
+                className="bg-destructive text-white hover:bg-destructive/80"
+              >
+                Reset
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+        {isAllGamesCompleted && (
+          <Link className="w-full" href="/claim-nft">
+            <Button variant={"secondary"} className="w-full">
+              Claim NFT
+            </Button>
+          </Link>
+        )}
+      </div>
     </div>
   );
 }
