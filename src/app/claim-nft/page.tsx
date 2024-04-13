8408645d947f78edcf4b24db68551f8f9db79f9c
Sean Gray <seangray.dev@gmail.com>
2024-04-13 14:45:14 -0400

    Add completion logic and UI for claiming NFT

--- src/app/claim-nft/page.tsx
+++ src/app/claim-nft/page.tsx
@@ -1,10 +1,54 @@
-import React from "react";
+"use client";
+
+import ErrorCompletion from "@/components/ui/error-completion";
+import Cookies from "js-cookie";
+import { ArrowLeftIcon } from "lucide-react";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
 
 export default function ClaimNFTPage() {
+  const [hasCompletedAllGames, setHasCompletedAllGames] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const gameWins = Cookies.get("gameWins");
+    const gameWinsData = gameWins ? JSON.parse(gameWins) : {};
+
+    const completedGamesCount =
+      Object.values(gameWinsData).filter(Boolean).length;
+
+    if (completedGamesCount === 5) {
+      setHasCompletedAllGames(true);
+    }
+
+    setIsLoading(false);
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <section className="flex w-full flex-1 flex-col items-center py-20">
+    <section className="container flex w-full flex-1 flex-col items-center justify-center py-20">
       <div>
-        <h2 className="mb-10 text-5xl font-bold">Congratulations!</h2>
+        {!hasCompletedAllGames ? (
+          <div className="flex flex-col items-center gap-2">
+            <Link className="flex items-center gap-1 hover:underline" href="/">
+              <ArrowLeftIcon size={18} />
+              Go back
+            </Link>
+            <ErrorCompletion />
+          </div>
+        ) : (
+          <>
+            <h2 className="mb-10 text-center text-5xl font-bold">
+              Congratulations!
+            </h2>
+            <p className="text-center">
+              You have completed all the games and can now claim your NFT!
+            </p>
+          </>
+        )}
       </div>
     </section>
   );
