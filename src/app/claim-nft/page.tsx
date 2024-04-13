2ecc225b713c150491914fae52bc815f8c95b26f
Sean Gray <seangray.dev@gmail.com>
2024-04-13 09:26:18 -0400

    Add ClaimNFTPage component

---
+++ src/app/claim-nft/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function ClaimNFTPage() {
+  return (
+    <section className="flex w-full flex-1 flex-col items-center py-20">
+      <div>
+        <h2 className="mb-10 text-5xl font-bold">Congratulations!</h2>
+      </div>
+    </section>
+  );
+}
