df5e1fa91fd8c132a1f65a4d176e14de24325333
Sean Gray <seangray.dev@gmail.com>
2024-04-13 09:25:48 -0400

    Update gameWins storage from localStorage to Cookies

--- src/lib/jotai/gameWins.ts
+++ src/lib/jotai/gameWins.ts
@@ -1,7 +1,8 @@
 import { atom } from "jotai";
+import Cookies from "js-cookie";
 
 function readGameWins() {
-  const item = localStorage.getItem("gameWins");
+  const item = Cookies.get("gameWins");
   return item ? new Map(JSON.parse(item)) : new Map();
 }
 
@@ -9,8 +10,7 @@ export const gameWinsAtom = atom(readGameWins(), (get, set, update) => {
   const newWins =
     typeof update === "function" ? update(get(gameWinsAtom)) : update;
   set(gameWinsAtom, newWins);
-  localStorage.setItem(
-    "gameWins",
-    JSON.stringify(Array.from(newWins.entries())),
-  );
+  Cookies.set("gameWins", JSON.stringify(Array.from(newWins.entries())), {
+    expires: 7,
+  });
 });
