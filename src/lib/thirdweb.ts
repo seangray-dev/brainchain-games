618bd2646c0dffd6d218c0e00b9ca93161032657
Sean Gray <seangray.dev@gmail.com>
2024-04-17 19:46:35 -0400

    export CONTRACT to reuse in app

--- src/lib/thirdweb.ts
+++ src/lib/thirdweb.ts
@@ -1,5 +1,6 @@
-import { createThirdwebClient, defineChain } from "thirdweb";
+import { createThirdwebClient, defineChain, getContract } from "thirdweb";
 import { sepolia } from "thirdweb/chains";
+import { CONTRACT_ABI } from "./contractAbi";
 
 const CLIENT_ID = process.env.NEXT_PUBLIC_TW_CLIENT_ID as string;
 
@@ -8,3 +9,13 @@ export const client = createThirdwebClient({
 });
 
 export const chain = defineChain(sepolia);
+
+export const CONTRACT_ADDRESS = "0x1d3cb7fa283395D6E8dA966dF97aC7Fc7fcEa969";
+export const CONTRACT_ID = "37154c8a-023b-47a4-9d05-b42bacac892a";
+
+export const CONTRACT = getContract({
+  client,
+  chain,
+  address: CONTRACT_ADDRESS,
+  // abi: CONTRACT_ABI,
+});
