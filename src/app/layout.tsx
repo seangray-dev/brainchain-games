411dd1f85a5ed8715804d63e45340d349e09af48
Sean Gray <seangray.dev@gmail.com>
2024-04-16 22:17:02 -0400

    Add ThirdwebProvider to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
 import CompletetionAnimation from "@/components/layout/completion-animation";
 import SiteNav from "@/components/layout/site-navigation";
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ThirdwebProvider } from "@/components/providers/thirdweb-provider";
 import { Toaster } from "@/components/ui/toaster";
 import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Instrument_Sans } from "next/font/google";
-import { ThirdwebProvider } from "thirdweb/react";
 import "./globals.css";
 
 const sans = Instrument_Sans({ subsets: ["latin"] });
